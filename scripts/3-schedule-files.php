bb603ec532783dd639b452f16a5a4af8ff3f9e9f
brechtvdv <br5cht@hotmail.com>
2015-07-16 18:31:29 +0200

    Clean up

--- scripts/3-schedule-files.php
+++ scripts/3-schedule-files.php
@@ -1,24 +1,23 @@
 <?php
 /**
- * This script populates the following GTFS files: routes.txt, trips.txt, calendar.txt, calendar_dates.txt, stop_times.txt
+ * This script populates the following GTFS files: routes.txt, trips.txt, stop_times.txt
  * @author Brecht Van de Vyvere <brecht@iRail.be>
  * @author Pieter Colpaert <pieter@iRail.be>
  * @license MIT
  */
 require("vendor/autoload.php");
+
 use iRail\brail2gtfs\RouteFetcher;
 
 $file_routes = "dist/routes.txt";
 $file_trips = "dist/trips.txt";
-$file_calendar = "dist/calendar.txt";
-$file_calendar_dates = "dist/calendar_dates.txt";
-$file_stop_times = "dist/stop_times";
+$file_stop_times = "dist/stop_times.txt";
 
-// Returns hashmap. Key: route - Value: array of all possible dates
+// Returns hashmap. Key: route_id - Value: array of dates with a distinct service_id
 function getRoutesWithDates() {
 	$hashmap = array(); // holds route_short_name => array of dates
 
-	if(($handle = fopen('dist/routes.tmp.txt', 'r')) !== false)
+	if(($handle = fopen('dist/routes_info.tmp.txt', 'r')) !== false)
 	{
 	    // get the first row, which contains the column-titles (if necessary)
 	    $header = fgetcsv($handle);
@@ -27,9 +26,13 @@ function getRoutesWithDates() {
 	    while(($line = fgetcsv($handle)) !== false)
 	    {
 			$route_short_name = $line[0]; // $line is an array of the csv elements
-			$date = $line[1];
+			$service_id = $line[1];
+			$date = $line[2];
 			
-			array_push($hashmap[$route_short_name], $date);
+			// Check if date with certain service_id has already been added
+			if (!checkForServiceId($hashmap[$route_short_name], $service_id) {
+				array_push($hashmap[$route_short_name], new array($date, $service_id));
+			}
 
 	        // I don't know if this is really necessary, but it couldn't harm;
 	        // see also: http://php.net/manual/en/features.gc.php
@@ -39,6 +42,21 @@ function getRoutesWithDates() {
 	}
 
 	return $hashmap;
+}
+
+function checkForServiceId($dateServiceIdPairs, $service_id) {
+	$contains = false;
+
+	foreach ($dateServiceIdPairs as $pair) {
+		// $date = $pair[0];
+		$id = $pair[1];
+		// Already added
+		if($service_id == $id) {
+			$contains = true;
+		}
+	}
+
+	return $contains;
 }
 
 function appendCSV($dist, $csv) {
@@ -67,35 +85,6 @@ function addTrip($trip) {
 	appendCSV($file_trips,$csv);
 }
 
-function addCalendar($calendar) {
-	$csv = "";
-	$csv .= $calendar["gtfs:service"] . ","; // service_id
-	$csv .= $calendar["gtfs:monday"] . ","; // monday
-	$csv .= $calendar["gtfs:tuesday"] . ","; // tuesday
-	$csv .= $calendar["gtfs:wednesday"] . ","; // wednesday
-	$csv .= $calendar["gtfs:thursday"] . ","; // thursday
-	$csv .= $calendar["gtfs:friday"] . ","; // friday
-	$csv .= $calendar["gtfs:saturday"] . ","; // saturday
-	$csv .= $calendar["gtfs:sunday"] . ","; // sunday
-	$csv .= $calendar["gtfs:startDate"] . ","; // start_date
-	$csv .= $calendar["gtfs:endDate"]; // end_date
-
-	global $file_calendar;
-	appendCSV($file_calendar,$csv);
-}
-
-function addCalendarDates($calendar_dates) {
-	foreach($calendar_dates as $calendar_date) {
-		$csv = "";
-		$csv .= $calendar_date["gtfs:service"] . ","; // service_id
-		$csv .= $calendar_date["dct:date"] . ","; // date
-		$csv .= $calendar_date["gtfs:dateAddition"]; // exception
-
-		global $file_calendar_dates;
-		appendCSV($file_calendar_dates,$csv);
-	}
-}
-
 function addStopTimes($stop_times) {
 	foreach($stop_times as $stop_time) {
 		$csv = "";
@@ -111,7 +100,7 @@ function addStopTimes($stop_times) {
 }
 
 function makeHeaders() {
-	global $file_routes, $file_trips, $file_calendar, $file_calendar_dates, $file_stop_times;
+	global $file_routes, $file_trips, $file_stop_times;
 
 	// routes.txt
 	$header = "route_id, agency_id, route_short_name, route_long_name,route_type";
@@ -121,56 +110,9 @@ function makeHeaders() {
 	$header = "route_id, service_id, trip_id";
 	appendCSV($file_trips, $header);
 
-	// calendar.txt
-	$header = "service_id, monday, tuesday, wednesday, thursday, friday, saturday, sunday, start_date, end_date";
-	appendCSV($file_calendar, $header);
-
-	// calendar_dates.txt
-	$header = "service_id,date,exception";
-	appendCSV($file_calendar_dates, $header);
-
 	// stop_times.txt
 	$header = "trip_id, arrival_time, departure_time, stop_id, stop_sequence";
 	appendCSV($file_stop_times, $header);
-}
-
-function checkCalendar($calendar, $date) {
-	$matchDayOfWeek = FALSE;
-	$matchTime = FALSE;
-
-	// Check if day of week matches calendar
-	$a = strptime($date, '%Y-%m-%d');
-	$timestamp = mktime(0, 0, 0, $a['tm_mon']+1, $a['tm_mday'], $a['tm_year']+1900);
-	$dayOfWeek = date("N", $timestamp); // ISO-8601 numeric representation of the day of the week
-
-	if($dayOfWeek == 1 && $calendar["gtfs:monday"] == 1) $matchDayOfWeek = TRUE;
-	else if($dayOfWeek == 2 && $calendar["gtfs:tuesday"] == 1) $matchDayOfWeek = TRUE;
-	else if($dayOfWeek == 3 && $calendar["gtfs:wednesday"] == 1) $matchDayOfWeek = TRUE;
-	else if($dayOfWeek == 4 && $calendar["gtfs:thursday"] == 1) $matchDayOfWeek = TRUE;
-	else if($dayOfWeek == 5 && $calendar["gtfs:friday"] == 1) $matchDayOfWeek = TRUE;
-	else if($dayOfWeek == 6 && $calendar["gtfs:saturday"] == 1) $matchDayOfWeek = TRUE;
-	else if($dayOfWeek == 7 && $calendar["gtfs:sunday"] == 1) $matchDayOfWeek = TRUE;
-
-	// Check that date is between start & end
-	// Convert to timestamp
-	$start_ts = strtotime($calendar["gtfs:startTime"]);
-	$end_ts = strtotime($calendar["gtfs:endTime"]);
-	$check_ts = strtotime($date);
-
-	if(($check_ts >= $start_ts) && ($check_ts <= $end_ts)) $matchTime = TRUE;
-
-	return $matchDayOfWeek && $matchTime;
-}
-
-function checkCalendarDates($calendar_dates, $date) {
-	$isException = FALSE;
-
-	foreach($calendar_dates as $calendar_date) {
-		if($calendar_date["dct:date"] == $date && $calendar_date["gtfs:dateAddition"]>0)
-			$isException = TRUE;
-	}
-
-	return $isException;
 }
 
 // header CSVs
@@ -178,13 +120,13 @@ makeHeaders();
 
 $hashmap_route_dates = getRoutesWithDates();
 
-foreach ($hashmap_route_dates as $route_short_name => $dates) {
+foreach ($hashmap_route_dates as $route_short_name => $dates_serviceId_pairs) {
 
 	while(count($dates)) {
 		$date = array_shift($dates);
 
 		// processor
-		list($route, $trips, $calendars, $calendar_dates_array, $stop_times_array) = RouteFetcher::fetchRoute($route_short_name, $date);
+		list($route, $trips, $stop_times_array) = RouteFetcher::fetchRoute($route_short_name, $date);
 
 		// content CSVs
 		addRoute($route);
@@ -192,25 +134,9 @@ foreach ($hashmap_route_dates as $route_short_name => $dates) {
         foreach ($trips as $trip) {
             addTrip($trip);
         }
-        
-        foreach ($calendars as $calendar) {
-            addCalendar($calendar);
-        }
-        
-        foreach ($calendar_dates_array as $calendar_dates) {
-            addCalendarDates($calendar_dates);
-        }
 
         foreach ($stop_times_array as $stop_times) {
             addStopTimes($stop_times);
         }
-
-		// delete dates from $dates with same calendar and calendar_dates
-		foreach ($dates as $date) {
-			if (checkCalendar($calendar, $date) || checkCalendarDates($calendar_dates, $date)) {
-                unset($date);
-            }
-        }
-        
 	}
 }
