f4ecb0d634a5f3b0034953b69b8ea4f00279c2a2
brechtvdv <br5cht@hotmail.com>
2015-07-16 23:37:45 +0200

    Fetch info about route. Generates routes.txt.

--- src/iRail/brail2gtfs/RouteFetcher.php
+++ src/iRail/brail2gtfs/RouteFetcher.php
@@ -9,7 +9,7 @@
  */
 namespace iRail\brail2gtfs;
 
-include_once ("../../../includes/simple_html_dom.php");
+include_once ("includes/simple_html_dom.php");
 
 use GuzzleHttp\Client;
 use Monolog\Logger;
@@ -20,43 +20,73 @@ class RouteFetcher {
     /**
      * Fetch Route fetches for a specific date an array of: a route object and stop_times
      */
-    static function fetchRouteAndStopTimes ($shortName, $date, $trip_id) {
-        $dateNMBS = date('Ymd', $date);
-        $serverData = getServerData($dateNMBS, $shortName);
+    static function fetchRouteAndStopTimes ($shortName, $date, $trip_id, $language) {
+        date_default_timezone_set('UTC');
 
-        list($route_entry, $stop_times) = self::fetchInfo($serverData, $shortName, $trip_id);
+        $dateNMBS = date_create_from_format('Ymd', $date)->format('d/m/Y');
+        $serverData = self::getServerData($dateNMBS, $shortName, $language);
 
+        list($route_entry, $stop_times) = self::fetchInfo($serverData, $shortName, $trip_id, $dateNMBS);
+        
         return [$route_entry, $stop_times];
     }
 
-    static function fetchInfo($serverData, $shortName, $trip_id) {
+    static function fetchInfo($serverData, $shortName, $trip_id, $date) {
         // create a log channel
         $log = new Logger('route_info');
         $log->pushHandler(new StreamHandler('route_info.log', Logger::ERROR));
 
-        $stops = array();
-        $html = str_get_html($serverData);
-        $nodes = $html->getElementById('tq_trainroute_content_table_alteAnsicht')->getElementByTagName('table')->children;
+        $route_entry = null;
+        $stopTimes = null;
 
-        if (is_object($nodes)) {
+        $html = str_get_html($serverData);
+
+        $test = $html->getElementById('tq_trainroute_content_table_alteAnsicht');
+        if (!is_object($test)) {
+            $log->addError('Error with fetching route: ' . $shortName . ' on ' . $date . "\n");
+        } else {
+
+            $nodes = $html->getElementById('tq_trainroute_content_table_alteAnsicht')->getElementByTagName('table')->children;
+
+            $stop_sequence = 1; // counter
+            $stopTimes = array();
+            $route_entry = array();
 
             // First node is just the header
             for($i=1; $i<count($nodes); $i++){
                 $node = $nodes[$i];
                 if(!count($node->attr)) continue; // row with no class-attribute contain no data
                 
-                // Todo                
-                $departureStation = "";
-                $arrivalStation = "";
+                // Arrival- and departuretimes
+                // First stop
+                if ($i == 1) {
+                    // only departure
+                    $departureTime = array_shift($node->children[1]->children[1]->nodes[0]->_);
+                    $arrivalTime = "";
+                    $departureStation = trim(array_shift($node->children[3]->nodes[0]->_));
+                } 
+                // Last stop
+                else if ($i == count($nodes)-1) {
+                    // only arrival
+                    $departureTime = "";
+                    $arrivalTime = array_shift($node->children[1]->children[0]->nodes[0]->_);
+                    $arrivalStation = trim(array_shift($node->children[3]->children[0]->nodes[0]->_));
+                } else {
+                    $departureTime = array_shift($node->children[1]->children[0]->nodes[0]->_);
+                    $arrivalTime = array_shift($node->children[1]->children[2]->nodes[0]->_);
+                    // Todo: station and platform
+                    // Todo: Find stop_id with stop_name
+                }
 
-                
-            }            
-        } else {
-            $log->addError('Caught exception: ' .  $e->getMessage() . "\n" . 'Route: ' . $shortName . "\n");
-            $route_entry = null;
+                // array_push($stopTimes, self::generateStopTimesEntry($trip_id, $arrival_time, $departure_time, $stop_id, $stop_sequence));
+
+                $stop_sequence++;
+            }
+
+            $route_entry = self::generateRouteEntry($shortName, $departureStation, $arrivalStation);
         }
 
-        return $route_entry;
+        return [$route_entry, $stopTimes];
     }
 
     static function generateRouteEntry($shortName, $departureStation, $arrivalStation) {
@@ -66,28 +96,32 @@ class RouteFetcher {
             "gtfs:longName" => $departureStation . " - " . $arrivalStation,
             "gtfs:shortName" => $shortName,
             "gtfs:agency" => "0",
-            "gtfs:routeType" => "gtfs:Rail", //→ 2 according to GTFS/CSV spec
+            "gtfs:routeType" => "gtfs:Rail" //→ 2 according to GTFS/CSV spec
         ];
 
         return $route_entry;
     }
 
-    static function generateStopTimesEntry() {
+    static function generateStopTimesEntry($trip_id, $arrival_time, $departure_time, $stop_id, $stop_sequence) {
+        $stoptimes_entry = [
+            "gtfs:trip" => $trip_id,
+            "gtfs:arrivalTime" => $arrival_time,
+            "gtfs:departureTime" => $departure_time,
+            "gtfs:stop" => $stop_id,
+            "gtfs:stopSequence" => $stop_sequence
+        ];
 
-    }
-
-    static function fetchStopTimes ($serverData, $trip_id) {
-        
-        return [];
+        return $stoptimes_entry;
     }
 
     // Scrapes a route of the Belgian Rail website
-    static function getServerData($date, $shortName) {
+    static function getServerData($date, $shortName, $language) {
         $request_options = array(
-                "timeout" => "30",
-                "useragent" => "iRail.be by Project iRail",
-                );
-        $scrapeURL = "http://www.belgianrail.be/jp/sncb-nmbs-routeplanner/trainsearch.exe/nn?ld=std&AjaxMap=CPTVMap&seqnr=1&";
+            "timeout" => "30",
+            "useragent" => "GTFS by Project iRail",
+        );
+
+        $scrapeURL = "http://www.belgianrail.be/jp/sncb-nmbs-routeplanner/trainsearch.exe/" . $language . "ld=std&seqnr=1&ident=at.02043113.1429435556&";
 
         $post_data = "trainname=" . $shortName . "&start=Zoeken&selectDate=oneday&date=" . $date . "&realtimeMode=Show";
 
