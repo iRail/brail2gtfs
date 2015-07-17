ae8e050cd2b0e10cb4a9b3f732e5549e905c5ea8
brechtvdv <br5cht@hotmail.com>
2015-07-17 16:32:49 +0200

    Add matching-algorithm for international stops

--- src/iRail/brail2gtfs/RouteFetcher.php
+++ src/iRail/brail2gtfs/RouteFetcher.php
@@ -24,16 +24,15 @@ class RouteFetcher {
         date_default_timezone_set('UTC');
 
         $dateNMBS = date_create_from_format('Ymd', $date)->format('d/m/Y');
-        // $dateNMBS = '17/07/2015';
-        // $shortName = 'ICE18';
+        
         $serverData = self::getServerData($dateNMBS, $shortName, $language);
 
-        list($route_entry, $stop_times) = self::fetchInfo($serverData, $shortName, $trip_id, $dateNMBS);
+        list($route_entry, $stop_times) = self::fetchInfo($serverData, $shortName, $trip_id, $dateNMBS, $language);
         
         return [$route_entry, $stop_times];
     }
 
-    static function fetchInfo($serverData, $shortName, $trip_id, $date) {
+    static function fetchInfo($serverData, $shortName, $trip_id, $date, $language) {
         var_dump($shortName);
         var_dump($date);
 
@@ -43,6 +42,7 @@ class RouteFetcher {
 
         $route_entry = null;
         $stopTimes = null;
+        $stations = null; // Used when there are stationnames that don't have a URL, so no stop_ids can be parsed
 
         $html = str_get_html($serverData);
 
@@ -72,25 +72,38 @@ class RouteFetcher {
                     // only departure
                     $departureTime = array_shift($node->children[1]->children[1]->nodes[0]->_);
                     $arrivalTime = "";
-                    $departureStation = trim(array_shift($node->children[3]->nodes[0]->_));
-                    if ($departureStation == "") {
-                        $departureStation = array_shift($node->children[3]->children[0]->nodes[0]->_);
+                    // Stop_name
+                    $stop_name = trim(array_shift($node->children[3]->nodes[0]->_));
+                    if ($stop_name == '') {
+                      // Stop_name has URL
+                      $stop_name = trim(array_shift($node->children[3]->children[0]->nodes[0]->_));
                     }
+
+                    $departureStation = $stop_name;
                 } 
                 // Last stop
                 else if ($i == count($nodes)-1) {
                     // only arrival
                     $departureTime = "";
                     $arrivalTime = array_shift($node->children[1]->children[0]->nodes[0]->_);
-                    $arrivalStation = trim(array_shift($node->children[3]->children[0]->nodes[0]->_));
+                    // Stop_name
+                    $stop_name = trim(array_shift($node->children[3]->nodes[0]->_));
+                    if ($stop_name == '') {
+                      // Stop_name has URL
+                      $stop_name = trim(array_shift($node->children[3]->children[0]->nodes[0]->_));
+                    }
+                    $arrivalStation = $stop_name;
                 } else {
                     $departureTime = array_shift($node->children[1]->children[0]->nodes[0]->_);
                     $arrivalTime = array_shift($node->children[1]->children[2]->nodes[0]->_);
+                    // Stop_name
+                    $stop_name = trim(array_shift($node->children[3]->nodes[0]->_));
+                    if ($stop_name == '') {
+                      // Stop_name has URL
+                      $stop_name = trim(array_shift($node->children[3]->children[0]->nodes[0]->_));
+                    }
                 }
-
-                // Stop_name
-                $stop_name = trim(array_shift($node->children[3]->nodes[0]->_)); 
-
+                
                 // Stop_id
                 // Can be parsed from the stop-URL
                 if (isset($node->children[3]->children[0])) {
@@ -100,9 +113,13 @@ class RouteFetcher {
                     $stop_id = 'http://irail.be/stations/NMBS/' . '00' . $nr;
                 } else {
                     // With foreign stations, there's a sometimes no URL available
-                    // Find the stop_id with the iRail/hyperrail API
+                    // Find the stop_id with the stations.json
+                    if ($stations == null) {
+                        $stations = self::getStations();
+                    }
 
-                    var_dump("stop id not found");
+                    $matches = self::getMatches($stations, $stop_name);
+                    $stop_id = self::getBestMatch($matches, $stop_name, $language);
                 }                
 
                 // Has platform
@@ -114,7 +131,7 @@ class RouteFetcher {
                     $platform = "";
                 }
 
-                // array_push($stopTimes, self::generateStopTimesEntry($trip_id, $arrival_time, $departure_time, $stop_id, $stop_sequence));
+                array_push($stopTimes, self::generateStopTimesEntry($trip_id, $arrivalTime, $departureTime, $stop_id, $stop_sequence));
 
                 $stop_sequence++;
                 $i++;
@@ -175,6 +192,154 @@ class RouteFetcher {
         curl_close ($ch);
 
         return $result;
+    }
+
+    static function getStations() {
+        $client = new Client();
+        $url = "https://irail.be/stations/NMBS";
+        $response = $client->get($url, [
+            'headers' => [
+                'Accept'     => 'application/json',
+            ]
+        ]);
+
+        $json = $response->getBody();
+
+        return json_decode($json, true);
+    }
+
+    // Stations as parameter, so we have to load it once
+    static function getMatches($stations, $query) {
+        // Hardcoded some stations that NMBS gives different names to
+        if ($query == 'Frankfurt Main (d)') {
+            $query = 'Frankfurt am Main Flughafen';
+        } else if ($query = 'Frankfurt Flugh (d)') {
+            $query = 'Frankfurt am Main Hbf';
+        }
+
+        // var_dump($stations);
+        $newstations = new \stdClass;
+        $newstations->{"@id"} = $stations["@id"];
+        $newstations->{"@context"} = $stations["@context"];
+        $newstations->{"@graph"} = array();
+        
+        //make sure something between brackets is ignored
+        $query = preg_replace("/\s?\(.*?\)/i", "", $query);
+        
+        // st. is the same as Saint
+        $query = preg_replace("/st(\s|$)/i", "(saint|st|sint) ", $query);
+        //make sure that we're only taking the first part before a /
+        $query = explode("/", $query);
+        $query = trim($query[0]);
+        
+        // Dashes are the same as spaces
+        $query = self::normalizeAccents($query);
+        $query = str_replace("\-", "[\- ]", $query);
+        $query = str_replace(" ", "[\- ]", $query);
+        
+        $count = 0;
+        foreach ($stations["@graph"] as $station) {
+            if (preg_match('/.*' . $query . '.*/i', self::normalizeAccents($station["name"]), $match)) {
+                $newstations->{"@graph"}[] = $station;
+                $count++;
+            } elseif (isset($station->alternative)) {
+                if (is_array($station->alternative)) {
+                    foreach ($station->alternative as $alternative) {
+                        if (preg_match('/.*(' . $query . ').*/i', self::normalizeAccents($alternative->{"@value"}), $match)) {
+                            $newstations->{"@graph"}[] = $station;
+                            $count++;
+                            break;
+                        }
+                    }
+                } else {
+                    if (preg_match('/.*' . $query . '.*/i', self::normalizeAccents($station->alternative->{"@value"}))) {
+                        $newstations->{"@graph"}[] = $station;
+                        $count++;
+                    }
+                }
+            }
+            if ($count > 5) {
+                return $newstations;
+            }
+        }
+        return $newstations;
+    }
+
+    /**
+     * @param $str
+     * @return string
+     * Languages supported are: German, French and Dutch
+     * We have to take into account that some words may have accents
+     * Taken from https://stackoverflow.com/questions/3371697/replacing-accented-characters-php
+     */
+    static function normalizeAccents($str)
+    {
+        $unwanted_array = array(
+            'Š' => 'S', 'š' => 's', 'Ž' => 'Z', 'ž' => 'z',
+            'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A',
+            'Ä' => 'A', 'Å' => 'A', 'Æ' => 'A', 'Ç' => 'C',
+            'È' => 'E', 'É' => 'E', 'Ê' => 'E', 'Ë' => 'E',
+            'Ì' => 'I', 'Í' => 'I', 'Î' => 'I', 'Ï' => 'I',
+            'Ñ' => 'N', 'Ò' => 'O', 'Ó' => 'O', 'Ô' => 'O',
+            'Õ' => 'O', 'Ö' => 'O', 'Ø' => 'O', 'Ù' => 'U',
+            'Ú' => 'U', 'Û' => 'U', 'Ü' => 'U', 'Ý' => 'Y',
+            'Þ' => 'B', 'ß' => 'Ss', 'à' => 'a', 'á' => 'a',
+            'â' => 'a', 'ã' => 'a', 'ä' => 'a', 'å' => 'a',
+            'æ' => 'a', 'ç' => 'c', 'è' => 'e', 'é' => 'e',
+            'ê' => 'e', 'ë' => 'e', 'ì' => 'i', 'í' => 'i',
+            'î' => 'i', 'ï' => 'i', 'ð' => 'o', 'ñ' => 'n',
+            'ò' => 'o', 'ó' => 'o', 'ô' => 'o', 'õ' => 'o',
+            'ö' => 'o', 'ø' => 'o', 'ù' => 'u', 'ú' => 'u',
+            'û' => 'u', 'ý' => 'y', 'ý' => 'y', 'þ' => 'b',
+            'ÿ' => 'y'
+        );
+        
+        return strtr($str, $unwanted_array);
+    }
+
+    static function getBestMatch($matches, $stop_name, $language) {
+        $i = 0; // Index to best match in the graph
+        $max_percent = 0; // Percentage of similarity of the best match
+
+        $j = 0; // Index to match in the loop
+        foreach ($matches->{"@graph"} as $match) {
+            // First check if the stationname is available in the same language
+            if (isset($match["alternative"])) {
+                $stationName = self::getAlternativeName($match["alternative"], $language);
+            } else {
+                $stationName = NULL;
+            }
+
+            if ($stationName == NULL) {
+                // Use the standardName
+                $stationName = $match["name"];
+            }
+
+            similar_text($stationName, $stop_name, $percent);
+            if ($percent > $max_percent) {
+                $i = $j;
+                $max_percent = $percent;
+            }
+
+            $j++;
+        }
+        
+        return $match["@id"];
+    }
+
+    static function getAlternativeName($stationsByLang, $language) {
+        // Dutch
+        if ($language == "nn") {
+            $language = "nl";
+        }
+
+        foreach ($stationsByLang as $s) {
+            if (isset($s["@language"])) {
+                return $s["@value"];
+            }
+        }
+
+        return NULL;
     }
 }
 
