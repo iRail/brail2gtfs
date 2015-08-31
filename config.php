d591eaedad80d0f4f7a6204daf9139db48995f2f
Tjoosten <Tjoosten3@gmail.com>
2015-08-31 22:52:57 +0200

    Removed commented code out of the config.php

--- config.php
+++ config.php
@@ -12,7 +12,4 @@ return array(
     'feed_version' => '1.0',
     'shortNames' => array("IC", "ICE", "L", "P", "TGV", "THA", "TRN", "EXT"),
     'language' => 'nl' // Dutch
-    // 'language' => 'en' // English
-    // 'language' => 'fr' // French
-    // 'language' => 'de' // German
 );
