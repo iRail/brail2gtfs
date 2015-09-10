f6912f5a6872bb20b8315d560135314fa0e80616
Tjoosten <Tjoosten3@gmail.com>
2015-09-11 00:10:22 +0200

    fixed dir path. -> scripts/0-agency.txt.php

--- scripts/0-agency.txt.php
+++ scripts/0-agency.txt.php
@@ -3,7 +3,7 @@
 $csv = 'agency_id,agency_name,agency_url,agency_timezone
 0,NMBS/SNCB,http://www.belgianrail.be/,Europe/Brussels';
 
-if (file_put_contents('dist/agency.txt', $csv)) {
+if (file_put_contents('../dist/agency.txt', $csv)) {
     echo "successfully wrote to dist/agency.txt\n";
 } else {
     echo "could not write dist/agency.txt\n";
