f5355ecee5ca8500ee0383d16fdd7524b0aca641
brechtvdv <br5cht@hotmail.com>
2015-11-05 17:04:49 +0100

    Add empty calendar.txt. Fix #58

---
+++ scripts/5-calendar.txt.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This script generates an empty calendar.txt file
+ *
+ * @author Brecht Van de Vyvere <brecht@iRail.be>
+ * @author Pieter Colpaert <pieter@iRail.be>
+ * @license MIT
+ */
+
+$file_calendar = 'dist/calendar.txt';
+
+function appendCSV($dist, $csv)
+{
+    file_put_contents($dist, trim($csv).PHP_EOL, FILE_APPEND);
+}
+
+// header CSV
+$header = 'service_id,monday,tuesday,wednesday,thursday,friday,saturday,sunday,start_date,end_date';
+appendCSV($file_calendar, $header);
