4d3667e1d4b23efb85156d9bc50a5efb961a8200
Pieter Colpaert <pieter.colpaert@okfn.org>
2016-07-24 10:48:28 +0200

    First step towards new GTFS deploy strategy of every 3 months

--- config.php
+++ config.php
@@ -1,17 +1,21 @@
-  <?php
+<?php
 
-  /*
-  * Possible traintypes: IC, ICE, L, P, TGV, THA, TRN, EXT, S*
-  * Possible languages: Dutch, English, French and German (currently only Dutch has been tested)
-  * Always leave one language uncommented
-  */
-  return [
-      'start_date'   => '08-12-2015',
-      'end_date'     => '20-12-2016',
-      'feed_version' => '1.0',
-      'shortNames'   => ['S1','S2','S3','S4','S5','S6','S7','S8','S9','S10','S20','S81','IC', 'ICE', 'L', 'P', 'TGV', 'THA', 'TRN', 'EXT'],
-      'language'     => 'nl', // Dutch
-      // 'language' => 'en' // English
-      // 'language' => 'fr' // French
-      // 'language' => 'de' // German
-  ];
+//We're going to start scraping the first three months starting the 15th of the month this file was created on
+$startDate = mktime(0, 0, 0, date("n"), 15, date("Y")); //15th of the current month
+$endDate = strtotime("+3 months", strtotime($startDate));
+
+/*
+ * Possible traintypes: IC, ICE, L, P, TGV, THA, TRN, EXT, S*
+ * Possible languages: Dutch, English, French and German (currently only Dutch has been tested)
+ * Always leave one language uncommented
+ */
+return [
+    'start_date'   => date("d-m-Y", $startDate),
+    'end_date'     => date("d-m-Y", $endDate),
+    'feed_version' => '1.0',
+    'shortNames'   => ['S1','S2','S3','S4','S5','S6','S7','S8','S9','S10','S20','S81','IC', 'ICE', 'L', 'P', 'TGV', 'THA', 'TRN', 'EXT'],
+    'language'     => 'nl', // Dutch
+    // 'language' => 'en' // English
+    // 'language' => 'fr' // French
+    // 'language' => 'de' // German
+];
